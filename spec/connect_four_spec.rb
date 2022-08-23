a3b774008212a48695a22788e345951f07d2adb0
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-08-22 19:15:45 -0700

    Add incomplete tests for #verify_input (not implemented yet)

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,1 +1,43 @@
 # lib/connect_four.rb
+
+require_relative './lib/connect_four.rb'
+
+describe ConnectFourGame do
+
+  describe '#player_input' do
+
+
+  end
+
+
+  # Purpose of this method
+  # Determine if the input column number is a valid number on the board
+  # Determine if the input column number has a free row to add a checker
+  describe '#verify_input' do 
+    
+
+    context 'when the board is empty' do
+      subject(:game_verify) { described_class.new }
+
+      it 'returns valid input when given valid column' do
+
+      end
+
+    end
+
+    context 'when a particular column is full' do
+      it 'returns nil when column is full' do
+
+      end
+    end
+
+    context 'when given an invalid column that is off the board' do
+      it 'returns nil' do 
+
+      end
+    end
+  end
+
+
+
+end
