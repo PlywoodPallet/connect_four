e736623e78e75a11b3bb18ccb8bac06fba6d1a83
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-08-22 19:14:52 -0700

    Add #initialize, #print_board

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -3,4 +3,27 @@
 # Notes
 # Connect Four board is 7 columns x 6 rows
 # Use unicode to display black or white checkers
-# use game design from one of the ruby_exercises
+# use TDD-friendly game design from ruby_exercises 14-15
+# user input is only a column number (1-7)
+
+class ConnectFourGame
+
+  def initialize(row = 6, col = 7)
+    @board = Array.new(row) { Array.new(col) }
+
+    # for IRB @board = Array.new(6) { Array.new(7) }
+  end
+
+  # return a string representation of the board
+  # TODO: have column numbers at the bottom for aid in user input
+  def print_board
+    output = ''
+    @board.each { |row| output.concat("#{row.join(' | ')} \n") }
+    output
+  end
+
+end
+
+
+a_game = ConnectFourGame.new
+a_game.print_board
