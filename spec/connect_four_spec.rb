76b1e599e980319909ad0d9dada45c76581cb537
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-08-22 20:03:46 -0700

    Basic tests for #player_move

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,15 +1,39 @@
 # lib/connect_four.rb
 
-require_relative './lib/connect_four.rb'
+require_relative '../lib/connect_four.rb'
 
 describe ConnectFourGame do
 
-  describe '#player_input' do
+  # Test a basic version of this method without error checking
+  describe '#player_move' do
+    subject(:game_move) { described_class.new }
 
+    it 'Adds player 1 checker in specific cell' do 
+      row = 0
+      col = 0
+      player_num = 1
+      
+      game_move.player_move(row, col, player_num)
+      board = game_move.instance_variable_get(:@board)
+      cell = board[row][col]
 
+      expect(cell).to eq('X')
+    end
+
+    it 'Adds player 2 checker in specific cell' do 
+      row = 0
+      col = 0
+      player_num = 2
+      
+      game_move.player_move(row, col, player_num)
+      board = game_move.instance_variable_get(:@board)
+      cell = board[row][col]
+
+      expect(cell).to eq('O')
+    end
   end
-
-
+  
+  
   # Purpose of this method
   # Determine if the input column number is a valid number on the board
   # Determine if the input column number has a free row to add a checker
@@ -19,20 +43,20 @@ describe ConnectFourGame do
     context 'when the board is empty' do
       subject(:game_verify) { described_class.new }
 
-      it 'returns valid input when given valid column' do
+      xit 'returns valid input when given valid column' do
 
       end
 
     end
 
     context 'when a particular column is full' do
-      it 'returns nil when column is full' do
+      xit 'returns nil when column is full' do
 
       end
     end
 
     context 'when given an invalid column that is off the board' do
-      it 'returns nil' do 
+      xit 'returns nil' do 
 
       end
     end
