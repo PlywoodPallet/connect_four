82be1c119c53d2beb235daab9cd93c0e07c30b87
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-08-29 18:43:43 -0700

    Move testing code to main.rb file

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -135,8 +135,3 @@ class ConnectFourGame
   end
 
 end
-
-
-a_game = ConnectFourGame.new
-# a_game.print_board
-a_game.play_game
---
+++ main.rb
@@ -0,0 +1,7 @@
+# main.rb
+
+require './lib/connect_four.rb'
+
+a_game = ConnectFourGame.new
+a_game.print_board
+a_game.play_game
